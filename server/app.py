cff9417448666bf67effae7a969456e3390cb6f4
Collins <collinskiarie13@outlook.com>
2023-09-21 16:25:07 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -20,19 +20,34 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(serialized_bakeries)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        serialized_bakery = bakery.to_dict(nested=True)
+        return jsonify(serialized_bakery)
+    else:
+        return jsonify({'message': 'Bakery not found'}), 404
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    serialized_baked_goods = [baked_good.to_dict() for baked_good in baked_goods]
+    return jsonify(serialized_baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive_good:
+        serialized_most_expensive_good = most_expensive_good.to_dict()
+        return jsonify(serialized_most_expensive_good)
+    else:
+        return jsonify({'message': 'No baked goods found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,9 +13,36 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    baked_goods = relationship('BakedGood', back_populates='bakery')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'baked_goods': [good.to_dict() for good in self.baked_goods]  # Serialize the related goods
+        }
+
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, ForeignKey('bakeries.id'), nullable=False)
+
+    bakery = relationship('Bakery', back_populates='baked_goods')
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'bakery_id': self.bakery_id
+        }
+
     
